2d6410b3c0c573d9dd60dc1af1295b93f1c404a4
sekisaii <145209193+sekisaii@users.noreply.github.com>
2024-02-19 19:37:01 +0530

    First Commit 🍥

---
+++ subtacs/src/test/java/com/subtacs/SubtacsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subtacs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubtacsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
